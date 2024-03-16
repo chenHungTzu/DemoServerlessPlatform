f16244129a997878bb56d60c0b419e267c7d14e6
anthonychen <motx2152000@gmail.com>
2024-03-17 03:16:47 +0800

    feat: modift

--- api/iac/lambda.tf
+++ api/iac/lambda.tf
@@ -44,6 +44,17 @@ resource "aws_iam_role_policy" "lambda_role_policy" {
 {
       "Version": "2012-10-17",
       "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeInstances",
+          "ec2:AttachNetworkInterface"
+        ],
+          "Resource": "*"
+      },
         {
          "Effect": "Allow",
          "Action": [
